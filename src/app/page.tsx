3a8b2319f24c413cf3b96c3917a3fc4d6a7737f8
Tyler Haisman <tyler@haisman.net>
2023-06-19 23:20:05 -0500

    Initializing database settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,28 +7,37 @@ import Planet from "../assets/images/planet.png"
 import Arrow from "../assets/images/arrow.png"
 import Link from "next/link";
 import Circle from "@/components/cirlce/Circle";
-// import { useRouter } from 'next/navigation'
-// import { signIn, signOut, useSession } from "next-auth/react"
-import SignIn from "@/components/signin/SignIn";
 
 export default function Home() {
-  // const router = useRouter()
-  // const { data: session } = useSession();
-
-  // if(session && session.user){
-  //   return (
-  //     <div>{session.user.email}</div>
-  //   )
-  // }
-
-  // const handleSubmit = async (event: FormEvent) => {
-  //   event.preventDefault();
-  //   router.push("/api/auth/signin");
-  //   const response = await fetch('/api/auth/signin');
-  //   const data = await response.json();
-  //   console.log(data); // Handle the response data as needed
-  // }
-
+  useEffect(() => {
+    const initDatabase = async () => {
+      await initializeDatabase();
+    }
+    initDatabase();
+  },[])
+  const initializeDatabase = async () => {
+    try {
+        const response = await fetch('/api/database', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                action: "createTables",
+            }),
+        });
+        const data = await response.json();
+        if (data.message == "Error reading message.") {
+            console.error("An error occured.");
+            return;
+        }
+        return true;
+    } catch (error) {
+        console.error(error);
+        console.error("An error occured.");
+        throw error;
+    }
+};
   return (
     <div className="home">
       <Circle></Circle>
@@ -48,64 +57,3 @@ export default function Home() {
     </div>
   )
 }
-
-
-// const createTables = async () => {
-//   fetch('/api/database', {
-//         method: 'POST',
-//         headers: {
-//             'Content-type': 'application/json',
-//         },
-//         body: JSON.stringify({ action: "createTables" }),
-//     })
-//         .then((response) => response.json())
-//         .then((data) => {
-//             console.log(data);
-//         })
-//         .catch((error) => {
-//             console.error(error);
-//         });
-// }
-
-// const addUser = async () => {
-//   fetch('/api/database', {
-//         method: 'POST',
-//         headers: {
-//             'Content-type': 'application/json',
-//         },
-//         body: JSON.stringify({
-//           action: "addUser",
-//           email: "tyler@haisman.net",
-//           password: "12345",
-//           username: "tylerhaisman",
-//           firstName: "Tyler",
-//           lastName: "Haisman",
-//         }),
-//     })
-//         .then((response) => response.json())
-//         .then((data) => {
-//             console.log(data);
-//         })
-//         .catch((error) => {
-//             console.error(error);
-//         });
-// }
-// const findUserByEmail = async () => {
-//   fetch('/api/database', {
-//         method: 'POST',
-//         headers: {
-//             'Content-type': 'application/json',
-//         },
-//         body: JSON.stringify({
-//           action: "findUserByEmail",
-//           email: "tyler@haisman.net",
-//         }),
-//     })
-//         .then((response) => response.json())
-//         .then((data) => {
-//             console.log(data);
-//         })
-//         .catch((error) => {
-//             console.error(error);
-//         });
-// }
